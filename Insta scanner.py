d6c2493aff9f983f3f72fd130e0a71cac6e4cc97
Udit Bhati <55918713+udit19281@users.noreply.github.com>
2022-10-29 22:51:17 +0530

    Update Insta scanner.py

--- Insta scanner.py
+++ Insta scanner.py
@@ -1,54 +1,85 @@
 from selenium import webdriver
 from time import sleep
+from selenium.webdriver.common.by import By
+
+
 
 class insta:
     def __init__(self,name,ps):
         self.name=name 
         self.ps=ps
-        self.driver='C:\\chromedriver'            #update Chrome driver path
-        self.bro=webdriver.Chrome(self.driver)
+        self.driver='C:\\chromedriver'    
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])      #update Chrome driver path
+        self.bro=webdriver.Chrome(self.driver, options=options)
         self.bro.get("https://instagram.com")
-        sleep(2)
-        seluser="/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[1]/div/label/input"
-        selpass="/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input"
-        sellogin="/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[1]/div[3]/button/div"
+        sleep(5)
+        seluser="/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[1]/div/label/input"
+        selpass="/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[2]/div/label/input"
+        sellogin="/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[3]"
         try:
-            self.bro.find_element_by_xpath(seluser).send_keys(name)
-            self.bro.find_element_by_xpath(selpass).send_keys(ps)
+            self.bro.find_element("xpath",seluser).send_keys(name)
+            self.bro.find_element("xpath",selpass).send_keys(ps)
             sleep(1)
-            self.bro.find_element_by_xpath(sellogin).click()
-            sleep(4)
+            self.bro.find_element("xpath",sellogin).click()
+            sleep(5)
             #uncomment if 2 factor is used
-            #input("press enter after code")
-            self.bro.find_element_by_xpath("/html/body/div[1]/section/main/div/div/div/div/button").click()
-            self.bro.get("https://www.instagram.com/"+name+"/")
+            input("Press enter after code")
+            # self.bro.find_element("xpath","/html/body/div[1]/section/main/div/div/div/div/button").click()
+            
         except Exception as e:
-            print(e)
             exit(0)
         
-    def username(self):
+    def username(self, scroll_box):
         sleep(1)
-        scroll_box = self.bro.find_element_by_xpath("/html/body/div[5]/div/div/div[2]")
         last_ht, ht = 0, 1
         while last_ht != ht:
             last_ht = ht
+            ht = self.bro.execute_script("""arguments[0].scrollTo(0, arguments[0].scrollHeight); return arguments[0].scrollHeight;""",scroll_box)
             sleep(3)
-            ht = self.bro.execute_script("""arguments[0].scrollTo(0, arguments[0].scrollHeight); return arguments[0].scrollHeight;""", scroll_box)
-        links = scroll_box.find_elements_by_tag_name('a')
+        links = scroll_box.find_elements(By.TAG_NAME,"a")
         names = [name.text for name in links if name.text != '']
-        self.bro.find_element_by_css_selector("body > div.RnEpo.Yx5HN > div > div > div:nth-child(1) > div > div:nth-child(3) > button > div > svg").click()
+        # self.bro.find_element("xpath","/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[1]/div/div[3]/div/button").click()
+        
         return names 
 
     def followers(self):
-        sleep(2)
-        self.bro.find_element_by_xpath("/html/body/div[1]/section/main/div/header/section/ul/li[2]/a").click()
-        foll=self.username()
+        
+        # self.bro.get("https://www.instagram.com/"+self.name+"/")
+        self.bro.get(f"https://www.instagram.com/{self.name}/followers/")
+        sleep(7)
+        # self.bro.find_element("xpath","/html/body/div[1]/section/main/div/header/section/ul/li[2]/a").click()
+        foll=[]
+        while True:
+            try:
+                scroll_box = self.bro.find_element("xpath","/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[2]")
+                foll=self.username(scroll_box)
+                break
+            except Exception as e:
+                print(e)
+                break
+                sleep(2)
+                pass    
+        
         return foll
+
     def following(self):
-        sleep(3)
-        self.bro.find_element_by_xpath("/html/body/div[1]/section/main/div/header/section/ul/li[3]/a").click()
-        f=self.username()
+        self.bro.get(f"https://www.instagram.com/{self.name}/following/")
+        sleep(7)
+        # self.bro.find_element("xpath","/html/body/div[1]/section/main/div/header/section/ul/li[3]/a").click()
+        f=[]
+        while True:
+            try:
+                scroll_box = self.bro.find_element("xpath","/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]")
+                f=self.username(scroll_box)
+                break
+            except Exception as e:
+                print(e)
+                break
+                sleep(2)
+                pass
         return f
+
     def dnfoll(self):
         followe=self.followers()
         foling=self.following()
@@ -59,9 +90,11 @@ if __name__ == "__main__":
     username=input("Enter Username: ")
     password=input("Enter Password: ")
     bot=insta(username,password)          
-
-    followers=bot.followers()
     following=bot.following()
+    print(following)
+    followers=bot.followers()
+    print(followers)
+    
     notfollowing=[u for u in following if u not in followers]
 
     print("\n\tpeople not following: " + str(len(notfollowing)))
